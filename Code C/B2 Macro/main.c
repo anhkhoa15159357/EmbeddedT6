9fdb475603049e863161ab0b1880628e034201c7
anhkhoa15159357 <136165537+anhkhoa15159357@users.noreply.github.com>
2023-06-13 11:40:23 +0700

    Update main.c

--- Code C/B2 Macro/main.c
+++ Code C/B2 Macro/main.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+#include "test.c"
 
+#define MAX 17
+#define SUM(a,b) a+b
+#define CREATE_FUNC(name_func, cmd)\
+void name_func()                    \
+{                                   \
+    printf("%s\n", (char *)cmd);       \
+}
+
+CREATE_FUNC(say,"Try my best");
+
+void display(){
+    printf("Hi everyone\n");
+}
+
+int main(int argc, char const *argv[])
+{
+    say();
+    display();
+    printf("MAX: %d\n", MAX);
+    printf("Sum: %d\n", SUM(10,7));
+    test();
+    return 0;
+}
